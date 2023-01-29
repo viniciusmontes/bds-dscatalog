2d0875d5177bbc7d94d79c50cc23b33666107eee
Vinicius Montes <viniciusmontes@outlook.com.br>
2023-01-29 01:16:51 -0300

    Prettier created

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,12 +1,8 @@
 import './assets/styles/custom.scss';
 import './App.css';
 
-
-
 function App() {
-  return (
-    <h1>Hello DSCatalog</h1>
-  );
+  return <h1>Hello DSCatalog</h1>;
 }
 
 export default App;
