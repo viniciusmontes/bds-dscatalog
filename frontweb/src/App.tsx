5a9a052553b60992c6eb02ed03fb0a4c168fbabc
Vinicius Montes <viniciusmontes@outlook.com.br>
2023-02-01 21:58:02 -0300

    Page Catalog

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,11 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Home from 'pages/Home';
+import Catalog from 'pages/Catalog';
 
 
 const App = () => {
   return (
-  <Home />
+  <Catalog/>
   );
 };
 
