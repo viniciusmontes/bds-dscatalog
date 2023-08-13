52661810ee16db10c5d453eb49a9bee3663e5c6f
viniciusmontes <viniciusmontes@outlook.com.br>
2023-08-13 15:33:01 -0300

    Catalog-ProductDetails links

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -2,15 +2,19 @@ import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
 
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 const ProductDetails = () => {
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
-        <div className="goback-container">
-          <ArrowIcon />
-          <h2>VOLTAR</h2>
-        </div>
+        <Link to="/products">
+          <div className="goback-container">
+            <ArrowIcon />
+
+            <h2>VOLTAR</h2>
+          </div>
+        </Link>
         <div className="row">
           <div className="col-xl-6">
             <div className="img-container">
@@ -27,7 +31,10 @@ const ProductDetails = () => {
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do produto</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Accusamus, esse!</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing
+                elit.Accusamus, esse!
+              </p>
             </div>
           </div>
         </div>
