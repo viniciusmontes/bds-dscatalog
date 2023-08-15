3313eac88a2e032d1ff6a572227128c3d7e9ee06
viniciusmontes <viniciusmontes@outlook.com.br>
2023-08-15 00:55:18 -0300

    React Hooks useState e useEffect

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -3,8 +3,22 @@ import ProductPrice from 'components/ProductPrice';
 
 import './styles.css';
 import { Link } from 'react-router-dom';
+import { Product } from 'types/product';
+import axios from 'axios';
+import { BASE_URL } from 'util/requests';
+import { useEffect, useState } from 'react';
 
 const ProductDetails = () => {
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    axios.get(BASE_URL + '/products/1').then((response) => {
+      setProduct(response.data);
+    });
+  }, []);
+
+  
+
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
@@ -19,21 +33,20 @@ const ProductDetails = () => {
           <div className="col-xl-6">
             <div className="img-container">
               <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
-                alt="Nome do produto"
+                src={product?.imgUrl}
+                alt={product?.name}
               />
             </div>
             <div className="name-price-container">
-              <h1>Nome do Produto</h1>
-              <ProductPrice price={2345.67} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do produto</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing
-                elit.Accusamus, esse!
+                {product?.description}
               </p>
             </div>
           </div>
