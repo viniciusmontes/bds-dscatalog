141cc980c2b328b970bebb76f55c2500611412cd
viniciusmontes <viniciusmontes@outlook.com.br>
2023-09-22 14:53:51 -0300

    Login request

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,20 +1,26 @@
 import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
+import { requestBackendLogin } from 'util/requests';
 
 import './styles.css';
 
 type FormData = {
-  username: string,
-  password: string
-}
+  username: string;
+  password: string;
+};
 
 const Login = () => {
-  
   const { register, handleSubmit } = useForm<FormData>();
-  const onSubmit = (formData : FormData) => {
-    console.log(formData);
-  }
+  const onSubmit = (formData: FormData) => {
+    requestBackendLogin(formData)
+      .then((response) => {
+        console.log('SUCESSO', response);
+      })
+      .catch((error) => {
+        console.log('ERRO', error);
+      });
+  };
 
   return (
     <div className="base-card login-card">
@@ -22,7 +28,7 @@ const Login = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
-          {...register("username")}
+            {...register('username')}
             type="text"
             className="form-control base-input"
             placeholder="Email"
@@ -31,7 +37,7 @@ const Login = () => {
         </div>
         <div className="mb-2">
           <input
-          {...register("password")}
+            {...register('password')}
             type="password"
             className="form-control base-input "
             placeholder="Password"
