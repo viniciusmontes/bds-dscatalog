7eaec0e78a77edeb9b3e719c0a249db771783105
viniciusmontes <viniciusmontes@outlook.com.br>
2023-09-25 19:23:57 -0300

    Form validation

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -43,6 +43,10 @@ const Login = () => {
           <input
             {...register('username', {
               required: 'Campo obrigatório',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Email inválido'
+              } 
             })}
             type="text"
             className="form-control base-input"
