34648aca4625e93336283c96933bc9428f98c869
Vinicius Montes <viniciusmontes@outlook.com.br>
2023-11-19 22:10:19 -0300

    Delete product, observer pattern

--- frontweb/src/components/ProductCard/index.tsx
+++ frontweb/src/components/ProductCard/index.tsx
@@ -2,12 +2,11 @@ import './styles.css';
 import ProductPrice from 'components/ProductPrice';
 import { Product } from 'types/product';
 
-
 type Props = {
   product: Product;
-}
+};
 
-const ProductCard = ({product} : Props) => {
+const ProductCard = ({ product }: Props) => {
   return (
     <div className="base-card product-card">
       <div className="card-top-container">
@@ -15,7 +14,7 @@ const ProductCard = ({product} : Props) => {
       </div>
       <div className="card-bottom-container">
         <h6>{product.name}</h6>
-        <ProductPrice price={product.price}/>
+        <ProductPrice price={product.price} />
       </div>
     </div>
   );
--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -12,6 +12,10 @@ const List = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
 
   useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = () => {
     const config: AxiosRequestConfig = {
       method: 'GET',
       url: '/products',
@@ -24,7 +28,7 @@ const List = () => {
       setPage(response.data);
       console.log(page);
     });
-  }, []);
+  };
 
   return (
     <div className="product-crud-container">
@@ -40,7 +44,7 @@ const List = () => {
       <div className="row">
         {page?.content.map((product) => (
           <div key={product.id} className="col-sm-6 col-md-12">
-            <ProductCrudCard product={product} />
+            <ProductCrudCard product={product} onDelete={() => getProducts()} />
           </div>
         ))}
       </div>
