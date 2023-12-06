398d37e93162870e5b60127197fc578f68d0b650
Vinicius Montes <viniciusmontes@outlook.com.br>
2023-12-06 12:08:03 -0300

    Creating props and onChange event in the Pagination component

--- frontweb/src/components/Pagination/index.tsx
+++ frontweb/src/components/Pagination/index.tsx
@@ -3,11 +3,17 @@ import ReactPaginate from 'react-paginate';
 
 import './styles.css';
 
-const Pagination = () => {
+type Props = {
+  pageCount: number;
+  range: number;
+  onChange?: (pageNumber: number) => void;
+};
+
+const Pagination = ({ pageCount, range, onChange }: Props) => {
   return (
     <ReactPaginate
-      pageCount={10}
-      pageRangeDisplayed={3}
+      pageCount={pageCount}
+      pageRangeDisplayed={range}
       marginPagesDisplayed={1}
       containerClassName="pagination-container"
       pageLinkClassName="pagination-item"
@@ -18,6 +24,7 @@ const Pagination = () => {
       disabledClassName="arrow-inactive"
       previousLabel={<ArrowIcon />}
       nextLabel={<ArrowIcon />}
+      onPageChange={(items) => (onChange ? onChange(items.selected) : {})}
     />
   );
 };
