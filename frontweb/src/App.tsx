f20cf1a4326c1f8f2376b41a87d963f93f988045
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-02-07 20:42:12 -0300

    Toast

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -3,17 +3,20 @@ import './App.css';
 import Routes from 'Routes';
 import { useState } from 'react';
 import { AuthContext, AuthContextData } from 'AuthContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   const [authContextData, setAuthContextData] = useState<AuthContextData>({
     authenticated: false,
   });
 
-  return(
-  <AuthContext.Provider value={{authContextData, setAuthContextData}}>
-    <Routes />
-  </AuthContext.Provider>
+  return (
+    <AuthContext.Provider value={{ authContextData, setAuthContextData }}>
+      <Routes />
+      <ToastContainer />
+    </AuthContext.Provider>
   );
-}
+};
 
 export default App;
--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -7,8 +7,10 @@ import Select from 'react-select';
 import { useEffect, useState } from 'react';
 import { Category } from 'types/category';
 import CurrencyInput from 'react-currency-input-field';
+import { toast } from 'react-toastify';
 
 import './styles.css';
+
 
 type UrlParams = {
   productId: string;
@@ -65,7 +67,10 @@ const Form = () => {
     };
 
     requestBackend(config).then(() => {
+      toast.info('Produto cadastrado com sucesso!');
       history.push('/admin/products');
+    }).catch(() => {
+      toast.error('Erro ao cadastrar produto!')
     });
   };
 
