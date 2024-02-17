809267186b47c396950b11d76e89cd3508a23b96
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-02-17 12:10:23 -0300

    ProductPrice, ProductCard tests

---
+++ frontweb/src/components/ProductCard/__tests__/ProductCard.spec.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import ProductCard from '..';
+import { Product } from 'types/product';
+
+
+test('should render ProductCard', () => {
+
+  const product : Product = {
+    name : 'DVD',
+    description: 'leitor de discos',
+    price: 150.1,
+    imgUrl: 'https://m.media-amazon.com/images/I/619ur+XXcUS._AC_UF1000,1000_QL80_.jpg',
+    
+  } as Product
+
+  render(<ProductCard product={product}/>);
+
+
+  screen.debug()
+
+  // espera que tenha no documento
+  expect(screen.getByText(product.name)).toBeInTheDocument();
+  expect(screen.getByAltText(product.name)).toBeInTheDocument();
+  expect(screen.getByText('R$')).toBeInTheDocument();
+  expect(screen.getByText('150,10')).toBeInTheDocument();
+});
---
+++ frontweb/src/components/ProductPrice/__tests__/ProductPrice.spec.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import ProductPrice from '..';
+import { formatPrice } from 'util/formatters';
+
+test('should render ProductPrice', () => {
+
+  const price = 10.1
+
+  render(<ProductPrice price={price} />);
+
+
+  screen.debug()
+
+  // espera que tenha no documento
+  expect(screen.getByText('R$')).toBeInTheDocument();
+  expect(screen.getByText('10,10')).toBeInTheDocument();
+});
